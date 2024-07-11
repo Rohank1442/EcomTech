aa7fd7b1525ac0a19ab804a3bad587a1afc88600
Rohank1442 <rohankumar1442@gmail.com>
2024-07-11 16:54:05 +0530

    check again

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import fontawesome from '@fortawesome/fontawesome'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Footer.css';
 const Footer = () => {
   return (
